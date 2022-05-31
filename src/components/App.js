3a50b7a6978a1cf2cfbd926a36010fdf641b840a
arunlalpp <arunlalpp@gmail.com>
2022-05-31 10:31:04 +0530

    routing

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { AuthProvider } from "../contexts/AuthContex";
 import Signup from "./Signup";
 import Dashboard from "./Dashboard";
 import Login from "./Login";
+import HomePage from "./HomePage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -16,7 +17,8 @@ function App() {
         <Router>
           <AuthProvider>
             <Routes>
-              <Route exact path="/" element={<Dashboard />} />
+            <Route exact path="/" element={<HomePage />} />
+              <Route path="/" element={<Dashboard />} />
               <Route path="/signup" element={<Signup />} />
               <Route path="/login" element={<Login />}/>
             </Routes>
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div>Dashboard
+
+    <Link to="/Homepage">logout</Link>
+    </div>
   )
 }
