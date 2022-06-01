c04b19a0d945efc5663c9eec9ffe580b2c7a88f0
arunlalpp <arunlalpp@gmail.com>
2022-06-01 15:14:13 +0530

    Card-container page style

--- src/components/Service.js
+++ src/components/Service.js
@@ -1,17 +1,23 @@
 import React from "react";
 import "./App.css";
 import NavLink from "./NavLink";
+import { BsFillPersonFill, BsHurricane, BsPersonPlusFill } from 'react-icons/bs';
 export default function ContainerCard() {
   return (
     <>
-    <NavLink />
+      <NavLink />
       <div className="wrapper-banner">
         <div className="banner-service-text">
           <span>powerful people experiences</span>
+          <div className="banner-short-text">
+            <h4>People <BsFillPersonFill /></h4>
+            <h4>Experience <BsHurricane /></h4>
+            <h4>Future <BsPersonPlusFill /></h4>
+          </div>
         </div>
       </div>
       <div className="wrapper-heading">
-      <h1>Our Services</h1>
+        <h1>Our Services</h1>
         <div className="container-card">
           <div class="card-container">
             <img
